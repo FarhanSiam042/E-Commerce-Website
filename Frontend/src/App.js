eb1a1eae182867a85f6d4d13662b7397e11ce620
Farhan Shahriar Haque <fsh33330@gmail.com>
2025-10-28 02:06:45 +0600

    Created Navbar

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,33 +1,7 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
-function Home() {
-  return (
-    <div className="home">
-      <h1>Welcome to Our E-Commerce Store</h1>
-      <p>Your one-stop shop for everything you need!</p>
-    </div>
-  );
-}
-
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <nav>
-            <h2>E-Commerce Store</h2>
-          </nav>
-        </header>
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
-  );
+  return <div></div>;
 }
 
 export default App;
