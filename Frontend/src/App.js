df9b020ff7575ce8d5010fd61bdfb1fd580e2e82
Farhan Shahriar Haque <fsh33330@gmail.com>
2025-10-30 02:35:49 +0600

    Created the top portion after navbar

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,30 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+
+function Home() {
+  return (
+    <div className="home">
+      <Hero />
+    </div>
+  );
+}
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ Frontend/src/Components/Items/Items.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Items.css";
+
+const Items = (props) => {
+  return (
+    <div className="items">
+      <img src={props.image} alt="" />
+      <p>{props.name}</p>
+      <div className="item-prices">
+        {props.new_price}
+
+        <div className="items-price-old">{props.old_price}</div>
+      </div>
+    </div>
+  );
+};
